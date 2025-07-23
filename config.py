607bf9fc6c526b284893dd11bd3037c7c9745d77
Milon Hossen <milonhosain382@gmail.com>
2025-07-23 19:07:55 +0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+API_ID = 22134923
+API_HASH = "d3e9d2f01d3291e87ea65298317f86b8"
+BOT_TOKEN = "8164105880:AAEwU1JkpAVr2PVFbmoyvkt2csKinfsChFw"
+OWNER_ID = 7383046042
+
+# Your API token and domain
+API_TOKEN = "04e8ee10b5f123456a640c8f33195abc"
+BASE_URL = "https://teraboxshortlink.hstn.me"
