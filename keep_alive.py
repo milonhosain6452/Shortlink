9ac1eee4716559ecb4c8398eccff476bc61bb777
Milon Hossen <milonhosain382@gmail.com>
2025-07-27 04:24:47 +0600

    Update and rename web.py to keep_alive.py

---
+++ keep_alive.py
@@ -0,0 +1,11 @@
+# keep_alive.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot is running!"
+
+def keep_alive():
+    app.run(host='0.0.0.0', port=8080)
