3851bcafa0d3371d48b85c6a1466ff7fef9a9fc1
Milon Hossen <milonhosain382@gmail.com>
2025-07-28 22:17:26 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,28 +1,23 @@
 <?php
 $filename = 'data.json';
 if (!file_exists($filename)) {
-    file_put_contents($filename, json_encode([
-        'links' => [],
-        'generated_count' => 0
-    ]));
+    header("Location: https://google.com");
+    exit;
 }
 
 $data = json_decode(file_get_contents($filename), true);
-$total_clicks = 0;
 
-foreach ($data['links'] as $link) {
-    $total_clicks += $link['clicks'];
+if (isset($_GET['r'])) {
+    $code = $_GET['r'];
+    foreach ($data['links'] as &$link) {
+        if ($link['code'] === $code) {
+            $link['clicks'] += 1;
+            file_put_contents($filename, json_encode($data, JSON_PRETTY_PRINT));
+            header("Location: " . $link['url']);
+            exit;
+        }
+    }
 }
 
+header("Location: https://google.com");
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Shortlink Stats</title>
-</head>
-<body>
-    <h1>Welcome to Shortlink Service</h1>
-    <p>Total shortlinks: <?= $data['generated_count'] ?></p>
-    <p>Total clicks: <?= $total_clicks ?></p>
-</body>
-</html>
